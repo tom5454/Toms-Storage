21e674c19d817cfa74deed2b4009b3b00a90f2de
tom5454 <tom5454a@gmail.com>
2020-04-27 10:04:21 +0200

    1.0.1 Update
    Fixed loot tables

--- src/main/java/com/tom/storagemod/StorageMod.java
+++ src/main/java/com/tom/storagemod/StorageMod.java
@@ -17,11 +17,7 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import com.tom.storagemod.block.BlockOpenCrate;
@@ -57,11 +53,11 @@ public class StorageMod
 		// Register the setup method for modloading
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 		// Register the enqueueIMC method for modloading
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+		//FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
 		// Register the processIMC method for modloading
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+		//FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
 		// Register the doClientStuff method for modloading
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+		//FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
 		// Register ourselves for server and other game events we are interested in
 		MinecraftForge.EVENT_BUS.register(this);
@@ -70,12 +66,11 @@ public class StorageMod
 	private void setup(final FMLCommonSetupEvent event)
 	{
 		// some preinit code
-		//LOGGER.info("HELLO FROM PREINIT");
-		//LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+		LOGGER.info("Tom's Storage Setup starting");
 		proxy.setup();
 	}
 
-	private void doClientStuff(final FMLClientSetupEvent event) {
+	/*private void doClientStuff(final FMLClientSetupEvent event) {
 		// do something that can only be done on the client
 		//LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
 		proxy.clientSetup();
@@ -91,16 +86,17 @@ public class StorageMod
 	private void processIMC(final InterModProcessEvent event)
 	{
 		// some example code to receive and process InterModComms from other mods
-		/*LOGGER.info("Got IMC {}", event.getIMCStream().
+		LOGGER.info("Got IMC {}", event.getIMCStream().
 				map(m->m.getMessageSupplier().get()).
-				collect(Collectors.toList()));*/
-	}
+				collect(Collectors.toList()));
+	}*/
 	// You can use SubscribeEvent and let the Event Bus discover methods to call
-	@SubscribeEvent
+
+	/*@SubscribeEvent
 	public void onServerStarting(FMLServerStartingEvent event) {
 		// do something when the server starts
 		//LOGGER.info("HELLO from server starting");
-	}
+	}*/
 
 	// You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
 	// Event bus for receiving Registry Events)
