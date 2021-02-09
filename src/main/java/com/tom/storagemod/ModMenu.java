d75d019d189affe286e146dcbf095ea5dcad143c
tom5454 <tom5454a@gmail.com>
2021-02-09 11:15:41 +0100

    1.1.5 Update
    Fixes #26

--- src/main/java/com/tom/storagemod/ModMenu.java
+++ src/main/java/com/tom/storagemod/ModMenu.java
@@ -1,21 +1,13 @@
 package com.tom.storagemod;
 
-import java.util.function.Function;
-
-import net.minecraft.client.gui.screen.Screen;
-
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 
 public class ModMenu implements ModMenuApi {
 
 	@Override
-	public String getModId() {
-		return "toms_storage";
-	}
-
-	@Override
-	public Function<Screen, ? extends Screen> getConfigScreenFactory() {
+	public ConfigScreenFactory<?> getModConfigScreenFactory() {
 		return screen -> AutoConfig.getConfigScreen(Config.class, screen).get();
 	}
 
