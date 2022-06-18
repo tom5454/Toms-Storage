ae0990932498b29c412ad9244ce30bcb388c2821
tom5454 <tom5454a@gmail.com>
2022-06-18 17:03:58 +0200

    1.3.4
    Fixed items not pulling from inventory (Closes #112)

--- src/main/java/com/tom/storagemod/jei/CraftingTerminalTransferHandler.java
+++ src/main/java/com/tom/storagemod/jei/CraftingTerminalTransferHandler.java
@@ -103,7 +103,7 @@ public class CraftingTerminalTransferHandler<C extends AbstractContainerMenu & I
 						for (int j = 0;j < stacks[i].length && k < 9;j++) {
 							if (stacks[i][j] != null && !stacks[i][j].isEmpty()) {
 								StoredItemStack s = new StoredItemStack(stacks[i][j]);
-								if(stored.contains(s)) {
+								if(stored.contains(s) || player.getInventory().findSlotMatchingItem(stacks[i][j]) != -1) {
 									CompoundTag tag = new CompoundTag();
 									stacks[i][j].save(tag);
 									CompoundNBT.put("i" + (k++), tag);
