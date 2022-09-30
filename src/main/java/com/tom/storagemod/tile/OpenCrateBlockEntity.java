a7f5575b97fb0e361193664328f713c14fd9e3b3
tom5454 <tom5454a@gmail.com>
2022-09-30 19:22:14 +0200

    1.4.3
    Fixed dupe bug with Open Crate (#153)

--- src/main/java/com/tom/storagemod/tile/OpenCrateBlockEntity.java
+++ src/main/java/com/tom/storagemod/tile/OpenCrateBlockEntity.java
@@ -1,13 +1,18 @@
 package com.tom.storagemod.tile;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+import net.fabricmc.fabric.api.transfer.v1.storage.StorageView;
+import net.fabricmc.fabric.api.transfer.v1.storage.base.SidedStorageBlockEntity;
+import net.fabricmc.fabric.api.transfer.v1.transaction.TransactionContext;
+import net.fabricmc.fabric.api.transfer.v1.transaction.base.SnapshotParticipant;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.ItemEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
@@ -18,8 +23,9 @@ import net.minecraft.util.math.Vec3d;
 import com.tom.storagemod.StorageMod;
 import com.tom.storagemod.TickerUtil.TickableServer;
 
-public class OpenCrateBlockEntity extends BlockEntity implements Inventory, TickableServer {
+public class OpenCrateBlockEntity extends BlockEntity implements SidedStorageBlockEntity, TickableServer {
 	private List<ItemEntity> items = new ArrayList<>();
+	private Handler handler = new Handler();
 
 	public OpenCrateBlockEntity(BlockPos pos, BlockState state) {
 		super(StorageMod.openCrateTile, pos, state);
@@ -36,74 +42,115 @@ public class OpenCrateBlockEntity extends BlockEntity implements Inventory, Tick
 	}
 
 	@Override
-	public int size() {
-		return items.size() + 1;
+	public Storage<ItemVariant> getItemStorage(Direction side) {
+		return handler;
 	}
 
-	@Override
-	public boolean isEmpty() {
-		return items.isEmpty();
+	private class State {
+		private ItemStack spawn;
+		private ItemEntity entity;
+		private int remove;
 	}
 
-	@Override
-	public ItemStack getStack(int index) {
-		if(items.size() > index){
-			ItemEntity ent = items.get(index);
-			if(ent.isAlive())return ent.getStack();
-			else return ItemStack.EMPTY;
-		}else return ItemStack.EMPTY;
-	}
+	private class Handler extends SnapshotParticipant<List<State>> implements Storage<ItemVariant> {
+		private List<State> state = new ArrayList<>();
 
-	@Override
-	public ItemStack removeStack(int index, int count) {
-		if(items.size() > index){
-			ItemEntity ent = items.get(index);
-			if(ent.isAlive()){
-				ItemStack s = ent.getStack().split(count);
-				if(ent.getStack().isEmpty())ent.discard();
-				return s;
+		@Override
+		protected List<State> createSnapshot() {
+			return new ArrayList<>(state);
+		}
+
+		@Override
+		protected void readSnapshot(List<State> snapshot) {
+			this.state = new ArrayList<>(snapshot);
+		}
+
+		@Override
+		protected void onFinalCommit() {
+			if(!state.isEmpty()) {
+				state.forEach(s -> {
+					if(s.spawn != null) {
+						BlockState state = world.getBlockState(pos);
+						Direction f = Direction.UP;
+						if(state.getBlock() == StorageMod.openCrate)f = state.get(Properties.FACING);
+						BlockPos p = pos.offset(f);
+						ItemEntity entityitem = new ItemEntity(world, p.getX() + 0.5, p.getY() + 0.5, p.getZ() + 0.5, s.spawn);
+						entityitem.setToDefaultPickupDelay();
+						entityitem.setVelocity(Vec3d.ZERO);
+						world.spawnEntity(entityitem);
+						items.add(entityitem);
+					} else if(s.entity != null) {
+						s.entity.getStack().split(s.remove);
+						if(s.entity.getStack().isEmpty())s.entity.discard();
+					}
+				});
 			}
-			else return ItemStack.EMPTY;
-		}else return ItemStack.EMPTY;
-	}
+			state = new ArrayList<>();
+		}
 
-	@Override
-	public ItemStack removeStack(int index) {
-		if(items.size() > index){
-			ItemEntity ent = items.get(index);
-			if(ent.isAlive()){
-				ItemStack s = ent.getStack();
-				ent.discard();
-				return s;
+		@Override
+		public long insert(ItemVariant resource, long maxAmount, TransactionContext transaction) {
+			if (maxAmount < 1) return 0;
+			updateSnapshots(transaction);
+			State state = new State();
+			state.spawn = resource.toStack((int) maxAmount);
+			this.state.add(state);
+			return maxAmount;
+		}
+
+		@Override
+		public long extract(ItemVariant resource, long maxAmount, TransactionContext transaction) {
+			if (maxAmount < 1)return 0;
+			long ext = 0;
+			for (ItemEntity e : new ArrayList<>(items)) {
+				if(resource.matches(e.getStack())) {
+					ext += extract0(e, (int) (maxAmount - ext), transaction);
+					if(ext == maxAmount)break;
+				}
 			}
-			else return ItemStack.EMPTY;
-		}else return ItemStack.EMPTY;
-	}
+			return ext;
+		}
 
-	@Override
-	public void setStack(int index, ItemStack stack) {
-		BlockState state = world.getBlockState(pos);
-		Direction f = Direction.UP;
-		if(state.getBlock() == StorageMod.openCrate)f = state.get(Properties.FACING);
-		BlockPos p = pos.offset(f);
-		ItemEntity entityitem = new ItemEntity(world, p.getX() + 0.5, p.getY() + 0.5, p.getZ() + 0.5, stack);
-		entityitem.setToDefaultPickupDelay();
-		entityitem.setVelocity(Vec3d.ZERO);
-		world.spawnEntity(entityitem);
-		items.add(entityitem);
-	}
+		private long extract0(ItemEntity ent, int maxAmount, TransactionContext transaction) {
+			if (maxAmount < 1)return 0;
+			if (ent == null)return 0;
+			updateSnapshots(transaction);
+			State state = new State();
+			state.entity = ent;
+			state.remove = Math.min(maxAmount, ent.getStack().getCount());
+			this.state.add(state);
+			return state.remove;
+		}
 
-	@Override
-	public boolean isValid(int index, ItemStack stack) {
-		return index == items.size();
-	}
+		@Override
+		public Iterator<StorageView<ItemVariant>> iterator() {
+			return items.stream().map(item -> (StorageView<ItemVariant>) new StorageView<ItemVariant>() {
 
-	@Override
-	public void clear() {
-	}
+				@Override
+				public long extract(ItemVariant resource, long maxAmount, TransactionContext transaction) {
+					return extract0(item, (int) maxAmount, transaction);
+				}
 
-	@Override
-	public boolean canPlayerUse(PlayerEntity paramPlayerEntity) {
-		return true;
+				@Override
+				public boolean isResourceBlank() {
+					return item.getStack().isEmpty();
+				}
+
+				@Override
+				public ItemVariant getResource() {
+					return ItemVariant.of(item.getStack());
+				}
+
+				@Override
+				public long getAmount() {
+					return item.getStack().getCount();
+				}
+
+				@Override
+				public long getCapacity() {
+					return 64;
+				}
+			}).iterator();
+		}
 	}
 }
