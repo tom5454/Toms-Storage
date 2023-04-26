0f005b0b3c7e8fd530a535bfdc204b18fe47935c
tom5454 <tom5454a@gmail.com>
2023-04-26 20:14:38 +0200

    23w17a port
    23w17a port

---
+++ Toms-Storage-Fabric-1.20/src/loader-shared/java/com/tom/storagemod/util/SimpleItemFilter.java
@@ -0,0 +1,66 @@
+package com.tom.storagemod.util;
+
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.item.ItemStack;
+
+public class SimpleItemFilter implements ItemPredicate {
+	private SimpleContainer filter;
+	private ItemStack stack;
+	private boolean matchNBT, allowList;
+
+	public SimpleItemFilter(ItemStack stack) {
+		this.stack = stack;
+		filter = new SimpleContainer(9);
+		CompoundTag tag = stack.getOrCreateTag();
+		filter.fromTag(tag.getList("Filter", 10));
+		matchNBT = tag.getBoolean("matchNBT");
+		allowList = tag.getBoolean("allowlist");
+	}
+
+	@Override
+	public boolean test(ItemVariant stack) {
+		return test0(stack) == allowList;
+	}
+
+	private boolean test0(ItemVariant stack) {
+		for (ItemStack f : filter.items) {
+			if(f.isEmpty())continue;
+			if(stack.isOf(f.getItem()) && (!matchNBT || stack.nbtMatches(f.getTag())))return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean configMatch(ItemStack stack) {
+		return stack == this.stack;
+	}
+
+	public void flush() {
+		CompoundTag tag = stack.getOrCreateTag();
+		tag.put("Filter", filter.createTag());
+		tag.putBoolean("matchNBT", matchNBT);
+		tag.putBoolean("allowlist", allowList);
+	}
+
+	public SimpleContainer getContainer() {
+		return filter;
+	}
+
+	public boolean isMatchNBT() {
+		return matchNBT;
+	}
+
+	public void setMatchNBT(boolean matchNBT) {
+		this.matchNBT = matchNBT;
+	}
+
+	public boolean isAllowList() {
+		return allowList;
+	}
+
+	public void setAllowList(boolean allowList) {
+		this.allowList = allowList;
+	}
+}
---
+++ Toms-Storage-Fabric-1.20/src/loader-shared/java/com/tom/storagemod/util/TagFilter.java
@@ -0,0 +1,77 @@
+package com.tom.storagemod.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.StringTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public class TagFilter implements ItemPredicate {
+	private ItemStack stack;
+	private boolean allowList;
+	private List<TagKey<Item>> tags;
+
+	public TagFilter(ItemStack stack) {
+		this.stack = stack;
+		CompoundTag tag = stack.getOrCreateTag();
+		allowList = tag.getBoolean("allowlist");
+		ListTag list = tag.getList("tags", Tag.TAG_STRING);
+		tags = new ArrayList<>();
+		try {
+			for (int i = 0; i < list.size(); i++) {
+				tags.add(TagKey.create(Registries.ITEM, new ResourceLocation(list.getString(i))));
+			}
+		} catch (Exception e) {
+		}
+	}
+
+	@Override
+	public boolean configMatch(ItemStack stack) {
+		return stack == this.stack;
+	}
+
+	@Override
+	public boolean test(ItemVariant stack) {
+		return test0(stack) == allowList;
+	}
+
+	@SuppressWarnings("deprecation")
+	private boolean test0(ItemVariant stack) {
+		for (int i = 0; i < tags.size(); i++) {
+			if(stack.getItem().builtInRegistryHolder().is(tags.get(i)))return true;
+		}
+		return false;
+	}
+
+	public boolean isAllowList() {
+		return allowList;
+	}
+
+	public void setAllowList(boolean allowList) {
+		this.allowList = allowList;
+	}
+
+	public void flush() {
+		CompoundTag tag = stack.getOrCreateTag();
+		tag.putBoolean("allowlist", allowList);
+		ListTag list = new ListTag();
+		tags.forEach(t -> list.add(StringTag.valueOf(t.location().toString())));
+		tag.put("tags", list);
+	}
+
+	public List<TagKey<Item>> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<ResourceLocation> tags) {
+		this.tags = tags.stream().map(t -> TagKey.create(Registries.ITEM, t)).toList();
+	}
+}
