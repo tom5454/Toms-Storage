961c833a3c062c1d38e68e9e832c6be4ca80bb34
tom5454 <tom5454a@gmail.com>
2023-05-24 16:31:18 +0200

    remove 1.20 from 1.19 branch

--- Toms-Storage-Fabric-1.20/src/main/java/com/tom/storagemod/platform/PlatformStorage.java
+++
@@ -1,8 +0,0 @@
-package com.tom.storagemod.platform;
-
-import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
-import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
-
-public interface PlatformStorage extends Storage<ItemVariant> {
-
-}
