86bb79d060b017ad8f44c37f670b91c5ca5c2acd
tom5454 <tom5454a@gmail.com>
2023-05-24 16:34:48 +0200

    remove 1.19 from 1.20 branch

--- Toms-Storage-119/src/loader-shared/java/com/tom/storagemod/network/OpenTerminalPacket.java
+++
@@ -1,4 +0,0 @@
-package com.tom.storagemod.network;
-
-public class OpenTerminalPacket {
-}
--- Toms-Storage-119/src/shared/java/com/tom/storagemod/block/IPaintable.java
+++
@@ -1,9 +0,0 @@
-package com.tom.storagemod.block;
-
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-
-public interface IPaintable {
-	boolean paint(Level world, BlockPos pos, BlockState to);
-}
