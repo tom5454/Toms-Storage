7f95bcb8e3e5bb23e42c1246a706a9d6952abe82
tom5454 <tom5454a@gmail.com>
2025-10-02 20:47:27 +0200

    simplify space efficiency sorter
    Fix IllegalArgumentException with the sorter

--- NeoForge/src/platform-shared/java/com/tom/storagemod/inventory/sorting/ComparatorSpaceEfficiency.java
+++ NeoForge/src/platform-shared/java/com/tom/storagemod/inventory/sorting/ComparatorSpaceEfficiency.java
@@ -6,20 +6,16 @@ import com.tom.storagemod.inventory.StoredItemStack;
 
 /**
  * Space efficiency is defined as the ability to store more items in fewer slots.
- * 
+ *
  * If I'm able to store 256 stone in 4 stacks, but I need 16 stacks for the same number of ender pearls,
  * then stone is more space-efficient.
  */
 public class ComparatorSpaceEfficiency implements Comparator<StoredItemStack> {
 	@Override
 	public int compare(StoredItemStack in1, StoredItemStack in2) {
-		// Fewer stacks = better
-		int stackCount = -Long.compare(in1.getStackCount(), in2.getStackCount());
-		// More items = better
-		int itemCount = Long.compare(in1.getQuantity(), in2.getQuantity());
-		// Larger stacks = better
-		int stackSize = Long.compare(in1.getMaxStackSize(), in2.getMaxStackSize());
+		int c = Long.compare(in1.getStackCount(), in2.getStackCount());
+		if (c != 0) return c;
 
-		return Integer.compare(Integer.compare(stackCount, itemCount), stackSize);
+		return in1.getDisplayName().compareTo(in2.getDisplayName());
 	}
 }
